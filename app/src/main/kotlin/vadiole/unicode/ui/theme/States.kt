ed5f20a333f0339ae20ba9b5fa922d15bddd55f1
vadiole <xdmitree@gmail.com>
2022-06-16 19:42:22 +0300

    add states constant pairs

---
+++ app/src/main/kotlin/vadiole/unicode/ui/theme/States.kt
@@ -0,0 +1,25 @@
+package vadiole.unicode.ui.theme
+
+val statesPressable = arrayOf(
+    intArrayOf(-android.R.attr.state_pressed),
+    intArrayOf(android.R.attr.state_pressed)
+)
+val keysDialogPressable = arrayOf(
+    key_dialogSurface,
+    key_dialogSurfacePressed
+)
+
+val keysWindowPressable = arrayOf(
+    key_windowSurface,
+    key_windowSurfacePressed
+)
+
+val keysSearchPressable = arrayOf(
+    key_searchFieldSurface,
+    key_searchFieldSurfacePressed
+)
+
+val keysWindowTextAccentPressable = arrayOf(
+    key_windowTextAccent,
+    key_windowTextAccentPressed
+)
