8886d5e69c4ce1e0624070168afd92cba2f5e82a
vadiole <xdmitree@gmail.com>
2022-06-17 11:55:40 +0300

    upgrade simple text view

--- app/src/main/kotlin/vadiole/unicode/ui/common/SimpleTextView.kt
+++ app/src/main/kotlin/vadiole/unicode/ui/common/SimpleTextView.kt
@@ -3,7 +3,11 @@ package vadiole.unicode.ui.common
 import android.content.Context
 import android.graphics.Canvas
 import android.graphics.Paint
+import android.graphics.Typeface
+import android.view.Gravity
 import android.view.View
+import vadiole.unicode.utils.extension.dp
+import kotlin.math.min
 
 open class SimpleTextView(context: Context) : View(context) {
     var text: String = ""
@@ -15,7 +19,7 @@ open class SimpleTextView(context: Context) : View(context) {
         get() = textPaint.textSize
         set(value) {
             textPaint.textSize = value
-            postInvalidate()
+            requestLayout()
         }
     var textColor: Int
         get() = textPaint.color
@@ -24,16 +28,62 @@ open class SimpleTextView(context: Context) : View(context) {
             postInvalidate()
         }
 
+    var gravity: Int = Gravity.CENTER
+        set(value) {
+            field = value
+            onSizeChanged(measuredWidth, measuredHeight, 0, 0)
+            invalidate()
+        }
+
+    var typeface: Typeface?
+        get() {
+            return textPaint.typeface
+        }
+        set(value) {
+            textPaint.typeface = value
+        }
+
     private val textPaint = Paint(Paint.ANTI_ALIAS_FLAG).apply {
         textAlign = Paint.Align.CENTER
+        textSize = 17f.dp(context)
+        letterSpacing = -0.02f
         isSubpixelText = true
     }
     private var textPositionY: Float = 0f
     private var textPositionX: Float = 0f
 
     override fun onSizeChanged(w: Int, h: Int, oldw: Int, oldh: Int) {
-        textPositionX = w / 2f
         textPositionY = 0.5f * (h - textPaint.descent() - textPaint.ascent())
+        when (gravity and Gravity.HORIZONTAL_GRAVITY_MASK) {
+            Gravity.CENTER_HORIZONTAL -> {
+                textPaint.textAlign = Paint.Align.CENTER
+                textPositionX = (w - paddingLeft - paddingRight) / 2f + paddingLeft
+            }
+            Gravity.LEFT -> {
+                textPaint.textAlign = Paint.Align.LEFT
+                textPositionX = paddingLeft.toFloat()
+            }
+            Gravity.RIGHT -> {
+                textPaint.textAlign = Paint.Align.RIGHT
+                textPositionX = w - paddingRight.toFloat()
+            }
+        }
+    }
+
+    override fun onMeasure(widthMeasureSpec: Int, heightMeasureSpec: Int) {
+        val widthMode = MeasureSpec.getMode(widthMeasureSpec)
+        val availableWidth = MeasureSpec.getSize(widthMeasureSpec)
+
+        // wrap content
+        if (widthMode == MeasureSpec.AT_MOST) {
+            val textWidth = textPaint.measureText(text).toInt()
+            val totalWidth = textWidth + paddingLeft + paddingRight
+            val finalWidth = min(availableWidth, totalWidth)
+            val newWidthMeasureSpec = MeasureSpec.makeMeasureSpec(finalWidth, MeasureSpec.EXACTLY)
+            setMeasuredDimension(newWidthMeasureSpec, heightMeasureSpec)
+        } else {
+            super.onMeasure(widthMeasureSpec, heightMeasureSpec)
+        }
     }
 
     override fun onDraw(canvas: Canvas) {
