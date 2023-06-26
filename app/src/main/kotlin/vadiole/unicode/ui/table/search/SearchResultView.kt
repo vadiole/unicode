2c2c787e3c0d18c274033ed27275f964e9f3a46c
vadiole <xdmitree@gmail.com>
2023-06-26 20:15:33 +0200

    fix search result view

--- app/src/main/kotlin/vadiole/unicode/ui/table/search/SearchResultView.kt
+++ app/src/main/kotlin/vadiole/unicode/ui/table/search/SearchResultView.kt
@@ -1,37 +1,23 @@
 package vadiole.unicode.ui.table.search
 
 import android.content.Context
-import android.graphics.Rect
-import android.graphics.drawable.ColorDrawable
 import androidx.recyclerview.widget.LinearLayoutManager
-import vadiole.unicode.UnicodeApp.Companion.themeManager
 import vadiole.unicode.ui.common.CollectionItemDecoration
 import vadiole.unicode.ui.common.CollectionView
-import vadiole.unicode.ui.theme.ThemeDelegate
-import vadiole.unicode.ui.theme.key_windowDivider
 import vadiole.unicode.utils.extension.dp
 
 class SearchResultView(
     context: Context,
     adapter: Adapter,
     private val delegate: Delegate,
-) : CollectionView(context), ThemeDelegate {
+) : CollectionView(context) {
 
     interface Delegate {
         fun onStartScrolling()
     }
 
     private val searchLayoutManager = LinearLayoutManager(context)
-    private val dividerDrawable = object : ColorDrawable() {
-        override fun getIntrinsicHeight(): Int {
-            return 1
-        }
 
-        override fun getPadding(padding: Rect): Boolean {
-            padding.set(16.dp(context), 0, 0, 0)
-            return true
-        }
-    }
     private val itemDecoration = CollectionItemDecoration(leftPadding = 14f.dp(context))
 
     init {
@@ -40,16 +26,11 @@ class SearchResultView(
         setItemViewCacheSize(8)
         setAdapter(adapter)
         addItemDecoration(itemDecoration)
-        themeManager.observe(this)
     }
 
     override fun onScrollStateChanged(state: Int) {
         if (state == SCROLL_STATE_DRAGGING) {
             delegate.onStartScrolling()
         }
-    }
-
-    override fun applyTheme() {
-        dividerDrawable.color = themeManager.getColor(key_windowDivider)
     }
 }
