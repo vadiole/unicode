885c86691680719e0aabc22e9242a5b061f6d46c
vadiole <xdmitree@gmail.com>
2024-01-23 23:49:55 +0100

    send duration

--- .github/workflows/publish-apk-to-telegram.js
+++ .github/workflows/publish-apk-to-telegram.js
@@ -6,7 +6,13 @@ module.exports = ({github, context}) => {
   const bot = new TelegramBot(token);
   const apkName = fs.readdirSync('./app/build/outputs/apk/debug/').filter(fn => fn.endsWith('.apk'))[0];
   const apk = fs.readFileSync('./app/build/outputs/apk/debug/' + apkName);
-  const caption = `${process.env.BRANCH_NAME}`;
+  const startTimestamp = `${process.env.START_TIMESTAMP}`;
+  console.log("Start timestamp: " + startTimestamp);
+  const nowTimestamp = Date.now();
+  console.log("Now timestamp: " + nowTimestamp);
+  const duration = nowTimestamp - startTimestamp;
+  console.log("Duration: " + duration);
+  const caption = `${process.env.BRANCH_NAME}`
   bot.sendDocument(chatId, apk, {caption: caption}, {filename: apkName, contentType: 'application/vnd.android.package-archive'})
     .then(response => {
       console.log("File " + apk + " successfully sent to Telegram");
