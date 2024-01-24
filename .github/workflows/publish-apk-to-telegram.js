12e5c2cf815ffe4446d9a6f7d3d43b378341662a
vadiole <xdmitree@gmail.com>
2024-01-24 01:36:33 +0100

    fix thumbnail format

--- .github/workflows/publish-apk-to-telegram.js
+++ .github/workflows/publish-apk-to-telegram.js
@@ -8,9 +8,9 @@ module.exports = ({github, context}) => {
   const bot = new TelegramBot(token);
   const apkName = fs.readdirSync('./app/build/outputs/apk/debug/').filter(fn => fn.endsWith('.apk'))[0];
   const apk = fs.readFileSync('./app/build/outputs/apk/debug/' + apkName);
-  const thumbnail = fs.readFileSync('./assets/logo-320.png');
+  const thumbnail = fs.readFileSync('./assets/logo-320.jpg');
   const duration = Date.now() - process.env.START_TIMESTAMP;
-  const messageOptions = {caption: `${process.env.BRANCH_NAME}\n[${formatter.timestampToMMSS(duration)}]`, thumbnail: thumbnail}
+  const messageOptions = {caption: `${process.env.BRANCH_NAME}\n[${formatter.timestampToMMSS(duration)}]`, thumb: thumbnail}
   const fileOptions = {filename: apkName, contentType: 'application/vnd.android.package-archive'};
   bot.sendDocument(chatId, apk, messageOptions, fileOptions)
     .then(response => {
