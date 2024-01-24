4eb467e61411e3f0186617b146b3e29b5ebbf56f
vadiole <xdmitree@gmail.com>
2024-01-24 01:04:22 +0100

    fix caption formatting

--- .github/workflows/publish-apk-to-telegram.js
+++ .github/workflows/publish-apk-to-telegram.js
@@ -8,14 +8,8 @@ module.exports = ({github, context}) => {
   const bot = new TelegramBot(token);
   const apkName = fs.readdirSync('./app/build/outputs/apk/debug/').filter(fn => fn.endsWith('.apk'))[0];
   const apk = fs.readFileSync('./app/build/outputs/apk/debug/' + apkName);
-  const startTimestamp = process.env.START_TIMESTAMP;
-  console.log("Start timestamp: " + startTimestamp);
-  const nowTimestamp = Date.now();
-  console.log("Now timestamp: " + nowTimestamp);
-  const duration = nowTimestamp - startTimestamp;
-  console.log("Duration: " + duration);
-  console.log("Duration formatted: " + formatter.timestampToMMSS(duration));
-  const messageOptions = {caption: `${process.env.BRANCH_NAME}\nin ${formatter.timestampToMMSS(duration)}`}
+  const duration = Date.now() - process.env.START_TIMESTAMP;
+  const messageOptions = {caption: `${process.env.BRANCH_NAME}\n[${formatter.timestampToMMSS(duration)}]`}
   const fileOptions = {filename: apkName, contentType: 'application/vnd.android.package-archive'};
   bot.sendDocument(chatId, apk, messageOptions, fileOptions)
     .then(response => {
