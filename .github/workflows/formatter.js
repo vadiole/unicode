10659001f9243fa14085be9846496fb6083c591b
vadiole <xdmitree@gmail.com>
2024-01-24 19:03:54 +0100

    remove js

--- .github/workflows/formatter.js
+++
@@ -1,5 +0,0 @@
-module.exports = {
-    timestampToMMSS: function(ms) {
-        return `${ms >= 60 * 1e3 ? Math.floor(ms / (60 * 1e3)) + 'm ' : ''}${Math.floor((ms % (60 * 1e3)) / 1e3)}s`;
-    }
-}
