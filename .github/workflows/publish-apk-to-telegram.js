909e17b6287786e93b6aa8691927c56fceab1471
vadiole <xdmitree@gmail.com>
2024-01-24 01:16:11 +0100

    add sending thumbnail

--- .github/workflows/publish-apk-to-telegram.js
+++ .github/workflows/publish-apk-to-telegram.js
@@ -8,9 +8,10 @@ module.exports = ({github, context}) => {
   const bot = new TelegramBot(token);
   const apkName = fs.readdirSync('./app/build/outputs/apk/debug/').filter(fn => fn.endsWith('.apk'))[0];
   const apk = fs.readFileSync('./app/build/outputs/apk/debug/' + apkName);
+  const thumbnail = fs.readFileSync('./app/src/assets/logo-320.png');
   const duration = Date.now() - process.env.START_TIMESTAMP;
   const messageOptions = {caption: `${process.env.BRANCH_NAME}\n[${formatter.timestampToMMSS(duration)}]`}
-  const fileOptions = {filename: apkName, contentType: 'application/vnd.android.package-archive'};
+  const fileOptions = {filename: apkName, thumbnail: thumbnail, contentType: 'application/vnd.android.package-archive'};
   bot.sendDocument(chatId, apk, messageOptions, fileOptions)
     .then(response => {
       console.log("File " + apkName + " successfully sent to Telegram.\nCaption: " + messageOptions.caption);
