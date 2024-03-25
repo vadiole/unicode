a498835449a547244d932da48b752e96b12cd045
vadiole <xdmitree@gmail.com>
2024-03-25 13:50:35 +0300

    fix crash handler

--- app/src/main/kotlin/vadiole/unicode/UncaughtExceptionHandler.kt
+++ app/src/main/kotlin/vadiole/unicode/UncaughtExceptionHandler.kt
@@ -4,31 +4,48 @@ import android.content.Context
 import android.content.Intent
 import android.os.Handler
 import android.os.Looper
+import vadiole.unicode.ui.SendCrashActivity
+import java.io.File
 import java.lang.Thread.UncaughtExceptionHandler
-import kotlin.system.exitProcess
 
 class UncaughtExceptionHandler(private val context: Context) : UncaughtExceptionHandler {
 
+    private val stackTraceDir = File(context.cacheDir, "stacktrace").also { it.mkdirs() }
+
     override fun uncaughtException(t: Thread, e: Throwable) {
-        e.printStackTrace()
         val mainLooper = Looper.getMainLooper()
         val isUiThread = mainLooper.thread == Thread.currentThread()
         if (isUiThread) {
-            startSendCrashActivity()
-            exitProcess(1)
+            handleException(e)
         } else {
             Handler(mainLooper).post {
-                startSendCrashActivity()
-                exitProcess(1)
+                handleException(e)
             }
         }
     }
 
-    private fun startSendCrashActivity() {
+    private fun handleException(e: Throwable) {
+        val outputText = e.stackTraceToString() + "\n\n\n" + getDebugInfo()
+        val stackTraceFile = File(stackTraceDir, "stacktrace.log").apply {
+            createNewFile()
+            writeText(outputText)
+        }
         val intent = Intent()
             .setAction("vadiole.unicode.SEND_CRASH")
-            .setFlags(Intent.FLAG_ACTIVITY_NEW_TASK)
+            .addFlags(Intent.FLAG_ACTIVITY_NEW_TASK or Intent.FLAG_ACTIVITY_CLEAR_TASK)
+            .addFlags(Intent.FLAG_ACTIVITY_EXCLUDE_FROM_RECENTS)
+            .putExtra(SendCrashActivity.STACKTRACE_FILE_PATH, stackTraceFile.canonicalPath)
+            .putExtra(SendCrashActivity.MAIN_PROCESS_PID, android.os.Process.myPid())
         context.startActivity(intent)
-        exitProcess(1)
+    }
+
+    private fun getDebugInfo(): String {
+        return "+------------------------------------------------------------+\n" +
+                "| Timestamp: ${System.currentTimeMillis()}\n" +
+                "| Version: ${BuildConfig.VERSION_NAME} (${BuildConfig.VERSION_CODE})\n" +
+                "| Device: ${android.os.Build.MANUFACTURER} ${android.os.Build.MODEL} (${android.os.Build.PRODUCT})\n" +
+                "| Android: ${android.os.Build.VERSION.RELEASE} (${android.os.Build.VERSION.SDK_INT})\n" +
+                "| Locales: ${context.resources.configuration.locales.toLanguageTags()}\n" +
+                "+------------------------------------------------------------+"
     }
 }
