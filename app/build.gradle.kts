b1767611974377276a5887382dbe797337349cce
vadiole <xdmitree@gmail.com>
2026-04-09 22:46:52 +0200

    bump version

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -12,8 +12,8 @@ android {
         applicationId = "vadiole.unicode"
         minSdk = 26
         targetSdk = 36
-        versionCode = 160
-        versionName = "1.6.0"
+        versionCode = 170
+        versionName = "1.7.0"
         base.archivesName.set("unicode-v$versionName")
     }
 
